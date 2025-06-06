48c2168214e391e4707e0b1cf2d116884186dbf5
Шашко Олег Владимирович <o.shashko@icloud.com>
2025-06-06 20:42:03 +0300

    initial commit

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -3,12 +3,15 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.util.concurrent.RateLimiter;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -23,7 +26,7 @@ public class CrptApi {
 
     static {
         try {
-            uri = new URI("https://javarush.com");
+            uri = new URI("https://ismp.crpt.ru/api/v3/lk/documents/create");
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
@@ -36,15 +39,18 @@ public class CrptApi {
 
     RateLimiter limiter;
 
+    public static String readFileToString(String path) throws IOException {
+        return new String(Files.readAllBytes(Path.of(URI.create(path))));
+    }
 
     public CrptApi(TimeUnit timeUnit, int requestLimit) {
         this.timeUnit = timeUnit;
         this.requestLimit = requestLimit;
         System.out.println(Duration.of(1, timeUnit.toChronoUnit()).toSeconds());
-        limiter = RateLimiter.create(requestLimit/Duration.of(1, timeUnit.toChronoUnit()).toSeconds());
+        limiter = RateLimiter.create(requestLimit / Duration.of(1, timeUnit.toChronoUnit()).toSeconds());
     }
 
-    public void accessApi(Map<String, String> requestBody) throws IOException, InterruptedException {
+    public void accessApi(String requestBody) throws IOException, InterruptedException {
         String body = mapper.writeValueAsString(requestBody);
         while(true) {
             doPost(body);
@@ -60,5 +66,10 @@ public class CrptApi {
                         .build();
         System.out.println("Called api, time is " + LocalDateTime.now());
         return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        CrptApi crptApi = new CrptApi(TimeUnit.SECONDS, 30);
+        crptApi.accessApi(readFileToString("classpath:/messageBody.json"));
     }
 }
